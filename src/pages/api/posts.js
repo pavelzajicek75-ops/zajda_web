56aa5424526fadef2572277d13d3938760142f89
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-14 20:52:19 +0200

    Create posts.js

---
+++ src/pages/api/posts.js
@@ -0,0 +1,20 @@
+export async function GET() {
+  const posts = [
+    {
+      id: 1,
+      title: "USA 2026 – Den 1",
+      slug: "usa-2026-den-1",
+      date: "2026-05-01"
+    },
+    {
+      id: 2,
+      title: "Island – Ledovce a vodopády",
+      slug: "island-ledovce-vodopady",
+      date: "2026-03-15"
+    }
+  ];
+
+  return new Response(JSON.stringify({ posts }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
