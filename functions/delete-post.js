d8ead84660fd84b3005d3b56102e67f1fdc1de29
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-14 21:14:57 +0200

    Create delete-post.js

---
+++ functions/delete-post.js
@@ -0,0 +1,8 @@
+export async function onRequestPost({ request }) {
+  const form = await request.formData();
+  const id = form.get("id");
+
+  console.log("TODO: smazat článek s ID:", id);
+
+  return Response.redirect("/admin/clanky", 302);
+}
