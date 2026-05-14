0661bddf0444d282edfa3bb6ce0e23dff44e2122
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-14 20:54:15 +0200

    Create photos.js

---
+++ src/pages/api/photos.js
@@ -0,0 +1,18 @@
+export async function GET() {
+  const photos = [
+    {
+      id: 1,
+      url: "https://placehold.co/600x400/orange/white?text=Foto+1",
+      alt: "Ukázková fotka 1"
+    },
+    {
+      id: 2,
+      url: "https://placehold.co/600x400/444/fff?text=Foto+2",
+      alt: "Ukázková fotka 2"
+    }
+  ];
+
+  return new Response(JSON.stringify({ photos }), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
