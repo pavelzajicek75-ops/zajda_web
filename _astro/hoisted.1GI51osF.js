2175657095047016b80f318005288eca1812c8ac
pavelzajicek75-ops <pavelzajicek75-ops@users.noreply.github.com>
2026-05-17 15:11:30 +0000

    deploy: ff23c9b0b5e48ca789f74387b17c5adcabc49a5f

--- _astro/hoisted.1GI51osF.js
+++
@@ -1,1 +0,0 @@
-import"https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js";const e=new Quill("#editor",{theme:"snow",placeholder:"Napište obsah článku..."});e.root.innerHTML="{article.content}";
