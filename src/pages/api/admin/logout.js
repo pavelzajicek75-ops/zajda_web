3b52369764f06456c40cfcb19a28380f74a74c5b
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-18 17:37:00 +0200

    Delete src/pages/api/admin directory

--- src/pages/api/admin/logout.js
+++
@@ -1,6 +0,0 @@
-export async function post() {
-  // Klient smaže token z localStorage
-  return new Response(JSON.stringify({ success: true }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
