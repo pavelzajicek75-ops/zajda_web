fa410977d577c267eb5218e09129ee018b189bf2
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-18 10:47:14 +0200

    Delete data directory

--- data/subsections.js
+++
@@ -1,7 +0,0 @@
-import subsectionsData from "../../data/subsections.json";
-
-export async function onRequestGet(context) {
-  return new Response(JSON.stringify(subsectionsData), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
