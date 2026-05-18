e51872b4d29d474d2817c20eeb08fc82c3da6aeb
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-18 10:24:19 +0200

    Delete functions directory

--- functions/api/articles.js
+++
@@ -1,7 +0,0 @@
-import articlesData from "../../data/articles.json";
-
-export async function onRequestGet(context) {
-  return new Response(JSON.stringify(articlesData), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
--- functions/api/sections.js
+++
@@ -1,8 +0,0 @@
-import sectionsData from "../../data/sections.json";
-
-export async function onRequestGet(context) {
-  return new Response(JSON.stringify(sectionsData), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
-
