84ea5d861e54172743ac677f1d7eb5a1c7212042
pavelzajicek75-ops <pavel.zajicek75@gmail.com>
2026-05-18 17:59:23 +0200

    Create r2.config.mjs

---
+++ r2.config.mjs
@@ -0,0 +1,9 @@
+// /r2.config.mjs
+
+export const R2_CONFIG = {
+  accountId: "TVŮJ_ACCOUNT_ID",
+  accessKeyId: "TVŮJ_ACCESS_KEY",
+  secretAccessKey: "TVŮJ_SECRET_KEY",
+  bucketName: "zajda-images",
+  endpoint: "https://TVŮJ_ACCOUNT_ID.r2.cloudflarestorage.com"
+};
